376f7ce8ae72ed34594e4624dd15aac02abe8180
thanhle <thanhle09t2bkdn@gmail.com>
2021-12-23 00:19:20 +0700

    Email

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\ApiController;
 use App\Http\Requests\Posts\PostCreateRequest;
 use App\Repositories\PostRepository;
+use App\Services\EmailService;
 
 class PostController extends ApiController
 {
@@ -16,18 +17,21 @@ class PostController extends ApiController
      * @var PostRepository
      */
     private $postRepository;
+    private $emailService;
 
     /**
      * Constructor.
      *
      * @param PostRepository $postRepository
      */
-    public function __construct(PostRepository $postRepository)
+    public function __construct(PostRepository $postRepository, EmailService $emailService)
     {
         $this->postRepository = $postRepository;
+        $this->emailService = $emailService;
     }
     public function store(PostCreateRequest $request)
     {
+        $this->emailService->send('thanhle09t2bkdn@gmail.com', 'test', 'test');
         return $this->successResponse('test');
     }
 }
---
+++ app/Services/EmailService.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Services;
+
+
+use Illuminate\Support\Facades\Mail;
+
+class EmailService
+{
+
+    public function send($to, $title, $description)
+    {
+        Mail::send('emails.subscription', ['title' => $title, 'description' => $description], function ($message) use ($to) {
+            $message->to($to, 'Visitor')->subject('Subscription Email');
+        });
+    }
+
+}
